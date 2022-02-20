3df3f31f293923936d0dbf19c0d53a40200ef67e
vikasagarwal1984 <vikasa.agarwal@gmail.com>
2022-02-20 21:39:39 +0530

    exported start function to be used in other projects

--- src/server.js
+++ src/server.js
@@ -3,27 +3,32 @@ const server = jsonServer.create();
 const router = jsonServer.router(require("./db.js")());
 const middlewares = jsonServer.defaults();
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
+function start() {
+  // Set default middlewares (logger, static, cors and no-cache)
+  server.use(middlewares);
 
-// Add custom routes before JSON Server router
-server.get("/echo", (req, res) => {
-  res.jsonp(req.query);
-});
+  // Add custom routes before JSON Server router
+  server.get("/echo", (req, res) => {
+    res.jsonp(req.query);
+  });
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  if (req.method === "POST") {
-    req.body.createdAt = Date.now();
-  }
-  // Continue to JSON Server router
-  next();
-});
+  // To handle POST, PUT and PATCH you need to use a body-parser
+  // You can use the one used by JSON Server
+  server.use(jsonServer.bodyParser);
+  server.use((req, res, next) => {
+    if (req.method === "POST") {
+      req.body.createdAt = Date.now();
+    }
+    // Continue to JSON Server router
+    next();
+  });
 
-// Use default router
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running on port 3000", "http://localhost:3000");
-});
+  // Use default router
+  server.use(router);
+
+  server.listen(3000, () => {
+    console.log("JSON Server is running on port 3000", "http://localhost:3000");
+  });
+}
+
+module.exports = { start };
